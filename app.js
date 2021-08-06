efb403a42a9f578ac43818fd9b27dc82112ebc24
Arash Alaei <arashalaei@aut.ac.ir>
2021-08-06 22:57:54 +0430

    prototype of tour & user routes

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
  * @author Arash Alaei <arashalaei22@gmail.com>. 
  */
 const express = require('express');
-
+const fs = require('fs');
+const path = require('path');
 const app = express();
+
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, './dev-data/data/tours-simple.json'),{encoding:'utf-8'}));
+
+// Global middleware
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.status(200).write(
@@ -15,6 +21,90 @@ app.get('/',(req, res) => {
 `);
 })
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+// Get all tours
+app.get('/api/v1/tours', (req, res) =>{
+    res.status(200).json({
+        status: 'success',
+        length: data.length,
+        data
+    })
+});
+
+// Get a tour
+app.get('/api/v1/tours/:id',(req, res) =>{
+    const id = +req.params.id;
+    res.status(200).json({
+        status: 'success',
+        data: data[id]
+    })
+})
+
+// Create new tour
+app.post('/api/v1/tours', (req, res) =>{
+    const id = data.length;
+    const {name = 'Spain', duration = 2} = req.body;
+    const newTour = Object.assign({id, name, duration}, {});
+    data.push(newTour);
+    fs.writeFileSync(path.join(__dirname, './dev-data/data/tours-simple.json'),JSON.stringify(data));
+    console.log(newTour);
+    res.status(201).json({
+        status: 'success',
+        tour: newTour
+    })
+});
+
+// Update the tour
+app.patch('/api/v1/tours/:id', (req, res) =>{
+    res.status(200).json({
+        status: 'success',
+        message: 'Not defined yet.'
+    })
+});
+
+// Delete the tour
+app.delete('/api/v1/tours/:id', (req, res) =>{
+    res.status(204).json({
+        status: 'success',
+        message: null
+    })
+});
+
+// Get all users
+app.get('/api/v1/users', (req, res) =>{
+    res.status(200).json({
+        status: 'success',
+        message: 'not defines yet!'
+    })
+});
+// Get a user
+app.get('/api/v1/users/:id', (req, res) =>{
+    res.status(200).json({
+        status: 'success',
+        message: 'not defines yet!'
+    })
+});
+// Create new user
+app.post('/api/v1/users/:id', (req, res) =>{
+    res.status(201).json({
+        status: 'success',
+        message: 'not defines yet!'
+    })
+});
+// update user
+app.patch('/api/v1/users/:id', (req, res) =>{
+    res.status(200).json({
+        status: 'success',
+        message: 'not defines yet!'
+    })
+});
+// delete user
+app.delete('/api/v1/users/:id', (req, res) =>{
+    res.status(204).json({
+        status: 'success',
+        message: null
+    })
+}); 
+const PORT = 3000;
+app.listen(PORT,'0.0.0.0', () => {
+    console.log(`Server is listening on port ${PORT}`);
 })
