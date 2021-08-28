072871c02ee6af2376f955b6edf93612474c483c
Arash Alaei <arashalaei@aut.ac.ir>
2021-08-28 22:46:09 +0430

    Security part: Set security HTTP headers by helmet, Limit requests from same API, limit Body parser,  Data sanitization against NoSQL query injection, Data sanitization against XSS and Prevent parameter pollution

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
  */
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/AppError');
@@ -10,11 +16,43 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100, 
+    windowMs: 60 * 60 * 1000, 
+    message: 'Too many requests from this IP, please try again in an hour!'
+})
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+            whitelist: [
+                'duration',
+                'ratingsQuantity',
+                'ratingsAverage',
+                'maxGroupSize',
+                'difficulty',
+                'price'
+            ]
+        })
+    );
+
 // Global middleware
-app.use(express.json());
 app.use((req, res, next) =>{
     console.log('From global middleware');
     next();
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,15 @@ const signToken = (userId) => jwt.sign({_id: userId}, process.env.JWT_SECRET_KEY
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
     user.password = undefined;
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ), 
+        httpOnly: true
+    }
+
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions);
     return res.status(statusCode).json({
         status: 'success', 
         token, 
